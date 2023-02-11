2c8f3c4b7bf135b1485b2653fcf4be1725038ff7
Birbseye <csabamisko@gmail.com>
2023-02-11 16:54:21 +0100

    Adding important fields

--- senior-java-spring-web-master_MiskoCsaba/src/main/java/hu/ponte/hr/domain/SignedImage.java
+++ senior-java-spring-web-master_MiskoCsaba/src/main/java/hu/ponte/hr/domain/SignedImage.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -17,6 +18,7 @@ public class SignedImage {
 
     private String name;
 
+    @Column(name = "media_type")
     private String mimeType;
 
     private long size;
@@ -24,9 +26,19 @@ public class SignedImage {
     @Column(columnDefinition = "TEXT", length = 100000)
     private String digitalSign;
 
+    @Column(name = "image_path")
     private String imagePath;
 
     @OneToMany(mappedBy = "signedImage")
     private List<ImageFile> imageFiles;
+
+    @Column(name = "date_created")
+    private LocalDateTime dateCreated = LocalDateTime.now();
+
+    @Column(name = "deleted_status")
+    private boolean isDeleted = false;
+
+    @Column(name = "date_deleted")
+    private LocalDateTime dateDeleted;
 
 }
